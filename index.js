59df9f19dbb9308e5b0a845ccf43e3665091687d
eemarishkin <ee.marishkin@yandex.ru>
2024-11-04 19:34:10 +0400

    modal_window_practic_2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const modal = mmodal()
+const fruits = [
+{id:1, title: 'яблоки', price: 20, img: 'https://avatars.mds.yandex.net/i?id=52322d308dab6d606ac65fe588dace807a1ecaec-9245818-images-thumbs&n=13'},
+{id:2, title: 'апельсины', price: 30, img: 'https://avatars.mds.yandex.net/i?id=7f777eb1172d662a9e95536fde325c6def5454d6-4571539-images-thumbs&n=13'},
+{id:3, title: 'манго', price: 40, img: 'https://otvet.imgsmail.ru/download/875a8375f91de049494d6073098e8a2f_dc3ce722ec066bbe769e70c64bc52ba8.png'}
+]
+
+
+
+
+const modal = $.modal({
+    title: 'marik modal',
+    closable: true,
+    content: `
+    <h4>Modal is working</h4>
+    <p>marik team</p>
+    `,
+    width: '400px',
+    footerbuttons: [
+      {text: 'Ok',type: 'primary', handler(){
+        console.log('primary btn clicked')
+        modal.close()
+      }},
+      {text: 'Cancel',type: 'denger', handler(){
+        console.log('denger btn clicked')
+        modal.close()
+      }}
+    ]
+})
--- plaguins/modal.js
+++ plaguins/modal.js
@@ -1,46 +1,102 @@
+Element.prototype.appendAfter = function(element){
+  element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop(){}
+
+
+function _createModalFooter(buttons=[]){
+ if(buttons.length===0){
+  return document.createElement('div')
+ }
+
+ const wrap = document.createElement('div')
+ wrap.classList.add('modal-footer')
+
+
+ buttons.forEach(btn => {
+  const $btn = document.createElement('button')
+  $btn.textContent = btn.text
+  $btn.classList.add('btn')
+  $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+  $btn.onclick= btn.handler || noop
+
+  wrap.appendChild($btn)
+ })
+
+ return wrap
+}
+
+
+
+
+
+
 function _createModal(options){
+   const DEFAULT_WIDTH = '600px'
    const modal = document.createElement('div')
    modal.classList.add('vmodal')
    modal.insertAdjacentHTML('afterbegin', `
-    <div class="modal__overlay">
-        <div class="modal__window">
+    <div class="modal__overlay" data-close="true">
+        <div class="modal__window" style="width: ${options.width || DEFAULT_WIDTH}">
             <div class="modal__header">
-                <span class="modal__title">Modal title</span>
-                <span class="modal__close">&times</span>
+                <span class="modal__title">${options.title || 'окно'}</span>
+                ${options.closable ? `<span class="modal__close" data-close="true">&times</span>` : ''}
             </div>
-            <div class="modal__body">
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
-            </div>
-            <div class="modal__footer">
-                <button>Ok</button>
-                <button>Cancel</button>
+            <div class="modal__body" data-content>
+                ${options.content || ''}
             </div>
         </div>
     </div>`)
+    const footer= _createModalFooter(options.footerbuttons)
+    footer.appendAfter(modal.querySelector('[data-content]'))
 document.body.appendChild(modal)
 return modal
 }
 
 
-  mmodal = function(options){
+  $.modal = function(options){
     const animationSpeed = 200
-    const gmodal= _createModal(options)
+    const $modal= _createModal(options)
     let closing=false
+    let destroyed = false
 
-    return {
-      open() {
-        !closing && gmodal.classList.add('open')
-      },
-      close(){
-        closing=true
-        gmodal.classList.remove('open')
-        gmodal.classList.add('hide')
-        setTimeout( ()=>{
-            gmodal.classList.remove('hide')
-            closing=false
-        },animationSpeed)
-      } ,
-      destroy(){}
+
+  const modal = {
+    open() {
+      if(destroyed){
+        return console.log('modal is destr')
+      }
+      !closing && $modal.classList.add('open')
+    },
+    close(){
+      closing=true
+      $modal.classList.remove('open')
+      $modal.classList.add('hide')
+      setTimeout( ()=>{
+          $modal.classList.remove('hide')
+          closing=false
+      },animationSpeed)
     }
+  }
+  
+  const listener = event =>{
+      if(event.target.dataset.close){
+        modal.close()
+      }
+  }
+
+
+    $modal.addEventListener('click', listener)
+
+    return Object.assign(modal, {
+      destroy(){
+        $modal.parentNode.removeChild($modal)
+        $modal.removeEventListener('click', listener)
+        destroyed = true
+      },
+      setContent(html){
+        $modal.querySelector('[data-content]').innerHTML=html
+      }
+    })
   }
